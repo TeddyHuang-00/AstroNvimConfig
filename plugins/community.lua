b846b5db12781f20e71ec96d841b7304150ca379
TeddyHuang-00 <huang_nan_2019@pku.edu.cn>
2023-12-28 18:24:17 -0500

    add toml to pack

--- plugins/community.lua
+++ plugins/community.lua
@@ -18,6 +18,7 @@ return {
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.programming-language-support.csv-vim" },

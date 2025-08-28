343bf7f5bc306a47f25be494e28c646fd8b193cd
Nan Huang <teddyhuangnan@gmail.com>
2025-08-28 01:13:07 -0400

    chore: Update config list

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,11 +1,11 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
 
 ---@type LazySpec
 return {
-  "meatballs/vim-xonsh",
+  -- "meatballs/vim-xonsh",
   -- -- == Examples of Adding Plugins ==
   --
   -- "andweeb/presence.nvim",
